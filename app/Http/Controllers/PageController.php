e56e2ff9f7ecb3bde2092f646d6dffd0b1246723
akristianto03 <akristianto03@student.ciputra.ac.id>
2023-04-29 01:37:24 +0700

    feat: dashboard

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Coa;
+use App\Models\Transaction;
 use App\Models\Umkm;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -19,6 +21,201 @@ class PageController extends Controller
 
     public function dashboard()
     {
-        return view('user.main.dashboard');
+        // aset lancar
+        $aset_lancar = Coa::where('category_id', '<=', 4)
+            ->where('umkm_id', Auth::user()->umkm->id)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            })->map->balance()->sum();
+
+        $liabilitas_pendek = Coa::where('category_id', '>=', 8)
+            ->where('category_id', '<=', 9)
+            ->where('umkm_id', Auth::user()->umkm->id)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            })->map->balance()->sum();
+
+        $total_aset = $aset_tetap = Coa::where('category_id', 5)
+            ->where('umkm_id', Auth::user()->umkm->id)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            })->map->balance()->sum() + $aset_lancar;
+
+        $laba_operasional = Coa::where('category_id', 12)
+            ->where('umkm_id', Auth::user()->umkm->id)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            })->map->balance()->sum() -
+            Coa::where('category_id', 13)
+            ->where('umkm_id', Auth::user()->umkm->id)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            })->map->balance()->sum() -
+            Coa::where('umkm_id', Auth::user()->umkm->id)
+            ->where(function ($q) {
+                $q->where('category_id', 14)
+                    ->orWhere('category_id', 16);
+            })
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            })->map->balance()->sum();
+
+
+        $pendapatan_lain = Coa::where('umkm_id', Auth::user()->umkm->id)
+            ->where('category_id', 15)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            });
+        $beban_lain = Coa::where('umkm_id', Auth::user()->umkm->id)
+            ->where('category_id', 16)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            });
+        $laba_bersih = $laba_operasional + $pendapatan_lain->map->balance()->sum() - $beban_lain->map->balance()->sum();
+        $laba_ditahan = $laba_bersih - $beban_lain = Coa::where('umkm_id', Auth::user()->umkm->id)
+            ->where('code', '3-30200')
+            ->get()->map->balance()->sum();
+
+        $liabilitas = Coa::where('category_id', '>=', 8)
+            ->where('category_id', '<=', 9)
+            ->where('umkm_id', Auth::user()->umkm->id)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            })->map->balance()->sum() +
+            Coa::where('category_id', 10)
+            ->where('umkm_id', Auth::user()->umkm->id)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            })->map->balance()->sum();
+
+        $modal_disetor = Coa::where('category_id', 11)
+            ->where('code', '3-30000')
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            })->map->balance()->sum() + Coa::where('category_id', 11)
+            ->where('code', '3-30001')
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            })->map->balance()->sum();
+
+
+        if ($total_aset != 0) {
+            $x1 = (($aset_lancar - $liabilitas) / $total_aset) * 6.56;
+            $x2 = 3.26 * ($laba_operasional / $total_aset);
+            $x3 = 6.72 * ($laba_operasional / $total_aset);
+        } else {
+            $x1 = 0;
+            $x2 = 0;
+            $x3 = 0;
+        }
+        if ($liabilitas != 0) {
+            $x4 = 1.05 * ($modal_disetor / $liabilitas);
+        } else {
+            $x4 = 0;
+        }
+
+        $zscore = $x1 + $x2 + $x3 + $x4;
+
+        if ($zscore > 2.6) {
+            $advices = [
+                "Pastikan kinerja keuangan UMKM tetap stabil dengan menjaga pengeluaran dan pemasukan tetap seimbang.",
+                "Cobalah untuk menambah jenis produk atau layanan yang ditawarkan untuk mengurangi risiko usaha jika salah satu produk atau layanan mengalami penurunan permintaan.",
+                "Lakukan promosi yang tepat sasaran dan hemat biaya untuk menarik lebih banyak pelanggan.",
+                "Hindari pembelian barang yang tidak diperlukan atau pemborosan biaya."
+            ];
+        } else if ($zscore < 1.8) {
+            $advices = [
+                "Segera evaluasi keuangan UMKM dan identifikasi masalahnya dengan mencari tahu rasio-rasio keuangan yang lemah.",
+                "Carilah bantuan keuangan dari pihak kreditur atau investor untuk membantu UMKM melewati masa-masa sulit tersebut.",
+                "Kurangi pengeluaran yang tidak penting dan tingkatkan pendapatan dengan cara memperluas pasar dan menawarkan diskon atau penawaran khusus untuk menarik lebih banyak pelanggan.",
+                "Cari cara baru untuk menghasilkan uang dengan mencari pasar baru atau menambah produk atau layanan yang ditawarkan."
+            ];
+        } else {
+            $advices = [
+                "Periksa kembali posisi keuangan UMKM dan cari cara untuk meningkatkan kinerja keuangan dengan cara mengevaluasi rasio-rasio keuangan yang lemah.",
+                "Cari tahu apa yang membuat pelanggan lebih tertarik pada produk atau layanan yang ditawarkan dan cobalah untuk meningkatkan kualitas produk atau layanan tersebut.",
+                "Kurangi biaya produksi dan operasional dengan cara memilih suplier yang lebih murah atau memanfaatkan teknologi untuk meningkatkan efisiensi produksi.",
+                "Bila mungkin, selalu sediakan cadangan dana untuk keperluan operasional UMKM agar dapat mengantisipasi risiko bisnis yang tidak terduga."
+            ];
+        }
+
+        $pendapatan = Coa::where('category_id', 12)
+            ->where('umkm_id', Auth::user()->umkm->id)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            });
+
+        // hitung laba rugi
+        $pendapatan = Coa::where('category_id', 12)
+            ->where('umkm_id', Auth::user()->umkm->id)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            });
+
+        $beban_pendapatan = Coa::where('category_id', 13)
+            ->where('umkm_id', Auth::user()->umkm->id)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            });
+
+        $beban_operasional = Coa::where('umkm_id', Auth::user()->umkm->id)
+            ->where(function ($q) {
+                $q->where('category_id', 14)
+                    ->orWhere('category_id', 16);
+            })
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            });
+
+        $pendapatan_lain = Coa::where('umkm_id', Auth::user()->umkm->id)
+            ->where('category_id', 15)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            });
+        $beban_lain = Coa::where('umkm_id', Auth::user()->umkm->id)
+            ->where('category_id', 16)
+            ->get()
+            ->filter(function ($q) {
+                return $q->balance() != 0;
+            });
+        $labarugi = $pendapatan->map->balance()->sum() -
+            $beban_pendapatan->map->balance()->sum() -
+            $beban_operasional->map->balance()->sum() +
+            ($pendapatan_lain->map->balance()->sum() - $beban_lain->map->balance()->sum());
+
+        $transactions = Transaction::where('umkm_id', Auth::user()->umkm->id)
+            ->where('category_id', '!=', 7)
+            ->where('category_id', '!=', 8)
+            ->get()->count();
+
+        $sales = Transaction::where('category_id', 1)
+            ->where('umkm_id', Auth::user()->umkm->id)
+            ->get();
+
+        return view('user.main.dashboard', compact(
+            'zscore',
+            'advices',
+            'pendapatan',
+            'labarugi',
+            'transactions',
+            'sales'
+        ));
     }
 }
--- resources/views/user/main/dashboard.blade.php
+++ resources/views/user/main/dashboard.blade.php
@@ -7,21 +7,133 @@
 
     <div class="py-6">
         <div class="max-w-7xl mx-auto px-4">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-10">
-                    <div class="flex flex-col items-center">
-                        <img src="{{ asset('images/assets/empty_state.svg') }}" class="w-44" alt="">
-                        <div class="mt-3">
-                            <h3 class="text-center font-bold text-lg">Anda belum memiliki catatan keuangan apapun</h3>
-                            <p class="text-center mt-2">Mulai mengatur konfigurasi invoice pertama anda.</p>
+            @if ($zscore == 0)
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-10">
+                        <div class="flex flex-col items-center">
+                            <img src="{{ asset('images/assets/empty_state.svg') }}" class="w-44" alt="">
+                            <div class="mt-3">
+                                <h3 class="text-center font-bold text-lg">Anda belum memiliki catatan keuangan apapun
+                                </h3>
+                                <p class="text-center mt-2">Mulai mengatur konfigurasi invoice pertama anda.</p>
+                            </div>
+                            <a href="{{ route('umkm.kas.create') }}"
+                                class="mt-3 inline-flex items-center px-4 py-2 bg-citradark-500 border border-transparent rounded-md font-bold text-xs text-white hover:bg-citradark-700 focus:bg-citradark-400 active:bg-citradark-600 focus:outline-none focus:ring-2 focus:ring-citradark-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                                Tambah Transaksi
+                            </a>
                         </div>
-                        <a href="{{ route('umkm.kas.create') }}"
-                            class="mt-3 inline-flex items-center px-4 py-2 bg-citradark-500 border border-transparent rounded-md font-bold text-xs text-white hover:bg-citradark-700 focus:bg-citradark-400 active:bg-citradark-600 focus:outline-none focus:ring-2 focus:ring-citradark-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                            Tambah Transaksi
-                        </a>
                     </div>
                 </div>
-            </div>
+            @else
+                <div class="bg-white overflow-hidden shadow-md sm:rounded-lg p-4">
+                    <div class="flex justify-between items-center">
+                        <h3 class="font-bold text-lg">{{ Auth::user()->umkm->name }}</h3>
+                        <div class="text-right">
+                            <p>
+                                kondisi kesehatan UMKM saat ini,
+                            </p>
+                            @if ($zscore > 2.6)
+                                <h1 class="text-2xl font-semibold text-citragreen-500">
+                                    Sehat
+                                </h1>
+                            @elseif ($zscore < 1.8)
+                                <h1 class="text-2xl font-semibold text-citrared-500">
+                                    Bangkrut
+                                </h1>
+                            @else
+                                <h1 class="text-2xl font-semibold text-citrayellow-500">
+                                    Berpotensi Bangkrut
+                                </h1>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-5 grid grid-cols-2 gap-4">
+                    <div class="flex gap-4">
+                        <div class="flex grow flex-col gap-4">
+                            <div class="w-full bg-white shadow-sm sm:rounded-md p-4">
+                                <div class="flex items-center gap-2">
+                                    <div
+                                        class="bg-citradark-100 rounded-full w-14 h-14 flex justify-items-center items-center">
+                                        <i class="text-citradark-500 bx bxs-purchase-tag text-3xl m-auto"></i>
+                                    </div>
+                                    <div>
+                                        <p class="">Total Penjualan</p>
+                                        <h2 class="text-2xl font-bold">
+                                            {{ AppHelper::rp($pendapatan->map->balance()->sum() ?? 0) }}
+                                        </h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full bg-white shadow-sm sm:rounded-md p-4">
+                                <div class="flex items-center gap-2">
+                                    <div
+                                        class="bg-{{ $labarugi >= 0 ? 'citragreen' : 'citrared' }}-100 rounded-full w-14 h-14 flex justify-items-center items-center">
+                                        <i
+                                            class="text-{{ $labarugi >= 0 ? 'citragreen' : 'citrared' }}-500 bx bxs-bar-chart-alt-2 text-3xl m-auto"></i>
+                                    </div>
+                                    <div>
+                                        <p class="">Total {{ $labarugi >= 0 ? 'Keuntungan' : 'Kerugian' }}</p>
+                                        <h2 class="text-2xl font-bold">
+                                            {{ AppHelper::rp($labarugi ?? 0) }}
+                                        </h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col gap-4">
+                            <div class="bg-white shadow-sm sm:rounded-md p-4">
+                                <div class="flex items-center gap-2">
+                                    <div
+                                        class="bg-citrablue-100 rounded-full w-14 h-14 flex justify-items-center items-center">
+                                        <i class="text-citrablue-500 bx bxs-wallet text-3xl m-auto"></i>
+                                    </div>
+                                    <div>
+                                        <p class="">Total Transaksi</p>
+                                        <h2 class="text-2xl font-bold">
+                                            {{ $transactions }}
+                                        </h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="bg-white shadow-sm sm:rounded-md p-4">
+                                <div class="flex items-center gap-2">
+                                    <div
+                                        class="bg-citrayellow-100 rounded-full w-14 h-14 flex justify-items-center items-center">
+                                        <i class="text-citrayellow-500 bx bxs-box text-3xl m-auto"></i>
+                                    </div>
+                                    <div>
+                                        <p class="">Produk Terjual</p>
+                                        <h2 class="text-2xl font-bold">
+                                            {{ $sales->map->details->sum('quantity') }}
+                                        </h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="bg-white shadow-md sm:rounded-lg p-4">
+                        <div class="mt-2">
+                            <h2 class="text-lg font-bold">
+                                Rekomendasi
+                            </h2>
+                            <p>
+                                Rekomendasi terkait kesehatan rasio keuangan UMKM milik anda.
+                            </p>
+                            <div class="mt-3 flex flex-col gap-3">
+                                @foreach ($advices as $advice)
+                                    <div class="p-3 bg-citragreen-100">
+                                        <p>{{ $advice }}</p>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
         </div>
     </div>
 </x-app-layout>
