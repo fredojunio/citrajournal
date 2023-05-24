a9ed546524e31e3c22701e5c95ed2ae3d69356a3
akristianto03 <akristianto03@student.ciputra.ac.id>
2023-05-24 14:44:25 +0700

    key

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+import fs from "fs";
+
+const host = "ezralfredo.com/citrajournal";
 
 export default defineConfig({
+    server: {
+        host,
+        hmr: { host },
+        https: {
+            key: fs.readFileSync(`keys/citrajournal.key`),
+            cert: fs.readFileSync(`keys/citrajournal.crt`),
+        },
+    },
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
